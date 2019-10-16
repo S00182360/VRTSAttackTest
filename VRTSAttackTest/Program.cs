b24f8ce459bd8c7511afe24bd43d514248d1a751
Samuel Colville <S00182360@mail.itsligo.ie>
2019-10-16 09:12:56 +0100

    Add project files.

---
+++ VRTSAttackTest/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace VRTSAttackTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ VRTSAttackTest/Unit.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VRTSAttackTest
+{
+    class Unit
+    {
+        public double HP { get; set; }
+        public double Attack { get; set; }
+        public double AttackModifier { get; set; }
+
+        public enum UnitType
+        {
+            Rock,
+            Paper,
+            Scissors,
+            Lizard,
+            Spock
+        }
+
+
+    }
+}
